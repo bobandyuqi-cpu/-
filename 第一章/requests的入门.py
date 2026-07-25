857dc88d66619dac79081a035e4244128d4b02dc
bobandyuqi <2305509591@qq.com>
2026-07-25 22:11:14 +0800

    增加了输入,可以让程序更灵活

--- 第一章/requests的入门.py
+++ 第一章/requests的入门.py
@@ -1,5 +1,6 @@
 import requests
-url="https://www.sogou.com/web?query=宋雨琦"
+query=input("请输入一个你喜欢的明星:")
+url=f"https://www.sogou.com/web?query={query}"
 
 dic={"user-agent":
 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36 Edg/150.0.0.0"}
