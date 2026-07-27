a4ab9e4e932b0b0d7447a03bd2ef27502784f94a
bobandyuqi <2305509591@qq.com>
2026-07-27 23:29:40 +0800

    添加了注释方便我学习理解

--- 第二章/re模块.py
+++ 第二章/re模块.py
@@ -4,24 +4,32 @@ from mplfinance import figure
 
 #findall: 匹配字符串中所有的符合正则的内容
 lit=re.findall(r"\d+","我的电话号是:10086,我女朋友的电话是10010")
+print("findall:")
 print(lit)
 
 #finditer:匹配字符串中所有的内容(返回的是迭代器),从迭代器中拿到内容需要.group()
 it=re.finditer(r"\d+","我的电话号码是:10086,我女朋友的电话号码是:10010")
+print("finditer:")
+print(it)
 for i in it:
     print(i.group())
 
 #search,找到一个结果就返回,返回的结果是match对象,拿数据需要.group()
 s=re.search(r"\d+","我的电话号码是10086,我女朋友的电话号码是:10010")
+print("search:")
+print(s)
 print(s.group())
 
 #match是从头开始匹配,相当于正则前面加个^
 s=re.match(r"\d+","10086,我女朋友的电话号码是:10010")
+print("match:")
+print(s)
 print(s.group())
 
 #预加载正则表达式,在compile里面最后加上re.S可以让.匹配换行符避免断开
 obj=re.compile(r"\d+")
 ret=obj.finditer("我的电话号码是:10086,我女朋友的电话是:10010")
+print("complile:")
 for i in ret:
     print(i.group())
 #案例1(?P<name>正则),可以单独从正则匹配的内容中进一步提取内容
